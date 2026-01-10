1899a8a8fb7cf2d6b8661c42849ef100b5ddbe40
Kiju <dev@jakubkijek.com>
2026-01-10 22:47:25 +0100

    feat(sim): BarnesHut

--- include/sim/type.hpp
+++ include/sim/type.hpp
@@ -9,27 +9,12 @@
 namespace nbody::sim {
 using namespace nbody::base::type;
 
-// TODO: Implement PointMassT.
-template <FloatT Float>
-struct PointMassT {
-    math::Vec2T<Float> pos{0.0, 0.0};
-    Float              mass{1.0};
-
-    std::string to_string() const {
-        return std::format("{{pos: {}, mass: {}}}", pos.to_string(), std::to_string(mass));
-    }
-};
-
 template <FloatT Float>
 struct BodyT {
-    PointMassT<Float>  pm;
+    math::Vec2T<Float> pos{0.0, 0.0};
     math::Vec2T<Float> vel{0.0, 0.0};
     math::Vec2T<Float> acc{0.0, 0.0};
-
-    std::string to_string() const {
-        return std::format("{{point_mass {}, vel: {}, acc: {}}}", pm.to_string(), vel.to_string(),
-                           acc.to_string());
-    }
+    Float              mass{0.0};
 };
 
 using BodyF32 = BodyT<F32>;
--- lib/sim/barnes_hut.cpp
+++ lib/sim/barnes_hut.cpp
@@ -1,395 +1,264 @@
-#include <algorithm>
-#include <limits>
-#include <memory>
-#include <span>
-#include <string>
-#include <tuple>
-#include <vector>
-
-#include "base/assert.hpp"
-#include "base/type.hpp"
-#include "math/impl.hpp"
 #include "sim/barnes_hut.hpp"
+
+#include "base/type.hpp"
 
 namespace nbody::sim {
 using namespace nbody::base::type;
 
 template <FloatT Float>
-BarnesHut<Float>::BarnesHut(const Config& config)
-    : m_bodies(std::move(config.bodies)),
-      m_integrate(std::move(config.integrate_fn)),
-      m_g(config.g),
-      m_softening(config.softening),
-      m_theta(config.theta),
-      m_depth(config.depth) {}
-
-template <FloatT Float>
-[[nodiscard]] std::span<const typename BarnesHut<Float>::Body, std::dynamic_extent>
-BarnesHut<Float>::bodies() const {
-    return m_bodies;
-}
-
-template <FloatT Float>
-void BarnesHut<Float>::insert_body(Body&& body) {
-    m_bodies.emplace_back(std::move(body));
-}
-
-template <FloatT Float>
-void BarnesHut<Float>::step(Float dt) {
-    // EXPLANATION:
-    // (1) Set all the previously updated accelerations to 0.
-    // (2) Create the root of the Quad Tree.
-    // (3) Construct the Quad Tree.
-    // (4) Apply the gravitaional force to all the bodies (calculate the aproximate acceleration).
-    // (5) Integrate the gravitational force (update the velocity and position).
-
-    for (Body& body : m_bodies) {
-        body.acc = Vec2::make_zero();
-    }
-
-    impl_create_root();
-    impl_construct_tree();
-    impl_apply_gravity();
-
-    for (Body& body : m_bodies) {
-        m_integrate(body, dt);
-    }
-}
-
-template <FloatT Float>
-std::vector<const typename BarnesHut<Float>::Node*> BarnesHut<Float>::collect_nodes() const {
-    std::vector<const Node*> stack;
-    std::vector<const Node*> nodes;
-
-    if (!m_root) return nodes;
-
-    stack.push_back(m_root.get());
-    nodes.reserve(m_bodies.size());
-
-    while (!stack.empty()) {
-        const Node* current = stack.back();
-        stack.pop_back();
-
-        if (!current) continue;
-        nodes.push_back(current);
-
-        for (USize qid = 0; qid < 4; ++qid) {
-            if (current->children[qid]) {
-                stack.push_back(current->children[qid].get());
-            }
-        }
-    }
-
-    return nodes;
-}
-
-// --- Quad Tree utils ---
-
-template <FloatT Float>
-void BarnesHut<Float>::impl_create_root() {
-    // NOTE: if no bodies were provided, create a minimal region root when there are no bodies.
-    if (m_bodies.empty()) {
-        m_root =
-            Node::make_ptr_region(Vec2::make_zero(), static_cast<Float>(M_MIN_ROOT_QUAD_RADIUS));
-        return;
-    }
-
-    auto [center, radius] = impl_root_node_center_radius();
-
-    // NOTE: Ensure radius is at least the minimum size. The simulation might go unstable if the
-    // provided radius is too small.
-    radius = std::max(radius, M_MIN_ROOT_QUAD_RADIUS);
-    m_root = Node::make_ptr_region(center, radius);
-}
-
-template <FloatT Float>
-void BarnesHut<Float>::impl_construct_tree() {
-    ASSERT(m_root, "Root is not initialized");
-
-    // NOTE: Insert each point mass into the quadtree.
-    for (const Body& body : m_bodies) {
-        m_root->insert_point_mass(body.pm, 0, m_depth);
-    }
-}
-
-template <FloatT Float>
-void BarnesHut<Float>::impl_apply_gravity() {
-    ASSERT(m_root, "Root is not initialized");
-
-    for (Body& body : m_bodies) {
-        m_root->apply_gravity_body(body, m_g, m_softening, m_theta);
-    }
-}
-
-template <FloatT Float>
-std::tuple<typename BarnesHut<Float>::Vec2, Float>
-BarnesHut<Float>::impl_root_node_center_radius() {
-    Float x_max = std::numeric_limits<Float>::lowest();
+typename BarnesHut<Float>::Quad BarnesHut<Float>::Quad::make_containing_bodies(
+    std::span<Body, std::dynamic_extent> bodies) {
     Float x_min = std::numeric_limits<Float>::max();
-    Float y_max = std::numeric_limits<Float>::lowest();
+    Float x_max = std::numeric_limits<Float>::lowest();
     Float y_min = std::numeric_limits<Float>::max();
+    Float y_max = std::numeric_limits<Float>::lowest();
 
-    for (const Body& body : m_bodies) {
-        x_max = std::max(x_max, body.pm.pos.x);
+    for (const Body& body : bodies) {
         x_min = std::min(x_min, body.pm.pos.x);
-        y_max = std::max(y_max, body.pm.pos.y);
+        x_max = std::max(x_max, body.pm.pos.x);
         y_min = std::min(y_min, body.pm.pos.y);
+        y_max = std::max(y_max, body.pm.pos.y);
     }
-
-    Float width  = x_max - x_min;
-    Float height = y_max - y_min;
-    Float radius = std::max(width, height) / 2.0;
-    radius       = std::max(radius, M_MIN_ROOT_QUAD_RADIUS);
 
     Vec2 center = Vec2{
-        static_cast<Float>(x_min + width / 2.0),
-        static_cast<Float>(y_min + height / 2.0),
+        static_cast<Float>(x_min + (x_max - x_min) / 2.0),
+        static_cast<Float>(y_min + (y_max - y_min) / 2.0),
     };
+    Float size = std::max(x_max - x_min, y_max - y_min);
 
-    return std::make_tuple(center, radius);
-}
-
-// --- Quad Tree Node ---
-
-template <FloatT Float>
-std::unique_ptr<typename BarnesHut<Float>::Node> BarnesHut<Float>::Node::make_ptr_empty(
-    const Vec2& qc, Float qr) {
-    std::unique_ptr<Node> node = std::make_unique<Node>();
-
-    node->quad_center = qc;
-    node->quad_radius = qr;
-    node->kind        = NodeKind::EMPTY;
-    node->mass        = 0.0;
-    node->center      = Vec2::make_zero();
-
-    return node;
+    return Quad{
+        .center = center,
+        .size   = size,
+    };
 }
 
 template <FloatT Float>
-std::unique_ptr<typename BarnesHut<Float>::Node> BarnesHut<Float>::Node::make_ptr_region(
-    const Vec2& qc, Float qr) {
-    std::unique_ptr<Node> node = std::make_unique<Node>();
+typename BarnesHut<Float>::Quad BarnesHut<Float>::Quad::into_quad(QuadIndex quad_index) const {
+    const Float half = size / 2.0;
 
-    node->quad_center = qc;
-    node->quad_radius = qr;
-    node->kind        = NodeKind::REGION;
-    node->mass        = 0.0;
-    node->center      = Vec2::make_zero();
-
-    return node;
+    switch (quad_index) {
+        case 0:  // NE
+            return Quad{
+                .center = Vec2{center.x + half / 2.0, center.y + half / 2.0},
+                .size   = half,
+            };
+        case 1:  // NW
+            return Quad{
+                .center = Vec2{center.x - half / 2.0, center.y + half / 2.0},
+                .size   = half,
+            };
+        case 2:  // SW
+            return Quad{
+                .center = Vec2{center.x - half / 2.0, center.y - half / 2.0},
+                .size   = half,
+            };
+        case 3:  // SE
+            return Quad{
+                .center = Vec2{center.x + half / 2.0, center.y - half / 2.0},
+                .size   = half,
+            };
+        default:
+            return Quad{
+                .center = Vec2::make_zero(),
+                .size   = 0.0,
+            };
+    }
 }
 
 template <FloatT Float>
-std::unique_ptr<typename BarnesHut<Float>::Node> BarnesHut<Float>::Node::make_ptr_leaf(
-    const Vec2& qc, Float qr, const PointMass& pm) {
-    std::unique_ptr<Node> node = std::make_unique<Node>();
+std::array<typename BarnesHut<Float>::Quad, 4> BarnesHut<Float>::Quad::into_quads() const {
+    Float half = size / 2.0;
 
-    node->quad_center = qc;
-    node->quad_radius = qr;
-    node->center      = pm.pos;
-    node->mass        = pm.mass;
-    node->kind        = NodeKind::LEAF;
+    return std::array<Quad, 4>{
+        Quad{
+             .center = Vec2{center.x + half / 2.0, center.y + half / 2.0},
+             .size   = half,
+             },
+        Quad{
+             .center = Vec2{center.x - half / 2.0, center.y + half / 2.0},
+             .size   = half,
+             },
+        Quad{
+             .center = Vec2{center.x - half / 2.0, center.y - half / 2.0},
+             .size   = half,
+             },
+        Quad{
+             .center = Vec2{center.x + half / 2.0, center.y - half / 2.0},
+             .size   = half,
+             },
+    };
+}
 
-    return node;
+template <FloatT Float>
+typename BarnesHut<Float>::Quad BarnesHut<Float>::Quad::quad_from_pos(Vec2 pos) const {
+    const bool is_north = pos.y >= center.y;
+    const bool is_east  = pos.x >= center.x;
+
+    if (is_north && is_east) {
+        return into_quad(0);
+    } else if (is_north && !is_east) {
+        return into_quad(1);
+    } else if (!is_north && !is_east) {
+        return into_quad(2);
+    } else {
+        return into_quad(3);
+    }
+}
+
+template <FloatT Float>
+BarnesHut<Float>::Node BarnesHut<Float>::Node::make_empty(NodeIndex next_index, Quad quad) {
+    return Node{
+        .children_index = NODE_INDEX_EMPTY,
+        .next_index     = next_index,
+        .quad           = quad,
+        .pos            = Vec2::make_zero(),
+        .mass           = 0.0,
+    };
 }
 
 template <FloatT Float>
 bool BarnesHut<Float>::Node::is_empty() const {
-    return kind == NodeKind::EMPTY;
+    return mass == 0.0;
 }
 
 template <FloatT Float>
-bool BarnesHut<Float>::Node::is_region() const {
-    return kind == NodeKind::REGION;
+bool BarnesHut<Float>::Node::is_inernal() const {
+    return children_index != NODE_INDEX_EMPTY;
 }
 
 template <FloatT Float>
 bool BarnesHut<Float>::Node::is_leaf() const {
-    return kind == NodeKind::LEAF;
+    return !is_empty() && !is_inernal();
 }
 
 template <FloatT Float>
-void BarnesHut<Float>::Node::insert_point_mass(const PointMass& pm, U16 current_depth,
-                                               U16 max_depth) {
-    // EXPLANATION:
-    // (1) If the node is EMPTY, it becomes a LEAF with it's center and mass represeting the point
-    // mass of a singular body. Return.
-    // (2) If the max recursion depth is reached combine the point mass of the quad with the
-    // provided point mass; recalculate the center of mass and mass of the quad. Return.
-    // (3) if the is REGION, detemine in which quadrant the provided point mass if ment to fit. If
-    // the quadrant is EMPTY, insert the point mass. If it is not, recursively call
-    // insert_point_mass on the matching node.
-    // (4) If the node is LEAF, subdivide it into 4 quads. Insert the provided point mass and insert
-    // the current node as a point mass to their respective nodes.
-
-    // NOTE: If node is EMPTY, it becomes a LEAF with this point mass.
-    if (is_empty()) {
-        kind   = NodeKind::LEAF;
-        center = pm.pos;
-        mass   = pm.mass;
-        return;
-    }
-
-    if (current_depth > max_depth) {
-        ASSERT(is_leaf(), "Is not NodeKind::LEAF");
-
-        Float new_mass = mass + pm.mass;
-        center.x       = (center.x * mass + pm.pos.x * pm.mass) / new_mass;
-        center.y       = (center.y * mass + pm.pos.y * pm.mass) / new_mass;
-        mass           = new_mass;
-        return;
-    }
-
-    if (is_region()) {
-        QuadId qid = impl_pos_quad_id(pm.pos);
-
-        if (!children[qid]) {
-            const Float child_qr = quad_radius / 2.0;
-            const Vec2  child_qc = impl_quad_id_center(qid);
-            children[qid]        = make_ptr_leaf(child_qc, child_qr, pm);
-        } else {
-            children[qid]->insert_point_mass(pm, current_depth + 1, max_depth);
-        }
-
-        self_recompute_com_mass();
-    } else if (is_leaf()) {
-        PointMass self_pm = self_as_point_mass();
-        kind              = NodeKind::REGION;
-
-        const Float child_qr = quad_radius / 2.0;
-        for (USize q = 0; q < 4; ++q) {
-            Vec2 child_qc = impl_quad_id_center(q);
-            children[q]   = make_ptr_empty(child_qc, child_qr);
-        }
-
-        QuadId self_qid = impl_pos_quad_id(self_pm.pos);
-        QuadId new_qid  = impl_pos_quad_id(pm.pos);
-
-        children[self_qid]->insert_point_mass(self_pm, current_depth + 1, max_depth);
-        children[new_qid]->insert_point_mass(pm, current_depth + 1, max_depth);
-
-        self_recompute_com_mass();
-    }
+void BarnesHut<Float>::QuadTree::clear(Quad quad) {
+    nodes.clear();
+    parent_indices.clear();
+    nodes.emplace_back(Node::make_empty(NODE_INDEX_EMPTY, quad));
 }
 
 template <FloatT Float>
-void BarnesHut<Float>::Node::apply_gravity_body(Body& body, Float g, Float softening,
-                                                Float theta) const {
-    if (is_empty()) {
-        return;
-    }
+typename BarnesHut<Float>::NodeIndex BarnesHut<Float>::QuadTree::subdivide(NodeIndex node_index) {
+    parent_indices.push_back(node_index);
 
-    if (is_leaf()) {
-        // NOTE: Directly apply contribution from this point mass.
-        impl_apply_gravity_body_source(body, self_as_point_mass(), g, softening);
-        return;
-    }
+    const NodeIndex children_index   = nodes.size();
+    nodes[node_index].children_index = children_index;
 
-    // NOTE: REGION node: decide whether to approximate or recurse; s is the size of the region
-    // (side length of the containg quad).
-    Float s     = quad_radius * 2.0;
-    Vec2  delta = center.sub(body.pm.pos);
-    Float dist  = delta.length();
-    Float ratio = s / dist;
-
-    if (ratio < theta) {
-        impl_apply_gravity_body_source(body, self_as_point_mass(), g, softening);
-    } else {
-        for (USize q = 0; q < 4; ++q) {
-            if (children[q]) {
-                children[q]->apply_gravity_body(body, g, softening, theta);
-            }
-        }
-    }
-}
-
-template <FloatT Float>
-std::string BarnesHut<Float>::Node::to_string() const {
-    std::string kind_str = (is_empty() ? "EMPTY" : (is_leaf() ? "LEAF" : "REGION"));
-    return "Node{" + kind_str + ", quad_center: " + center.to_string() +
-           ", quad_radius: " + std::to_string(quad_radius) + ", mass: " + std::to_string(mass) +
-           "}";
-}
-
-template <FloatT Float>
-BarnesHut<Float>::PointMass BarnesHut<Float>::Node::self_as_point_mass() const {
-    return PointMass{
-        .pos  = center,
-        .mass = mass,
+    const std::array<NodeIndex, 4> next_indices = {
+        children_index + 1,
+        children_index + 2,
+        children_index + 3,
+        nodes[node_index].next_index,
     };
+
+    const std::array<Quad, 4> quads = nodes[node_index].quad.into_quads();
+
+    for (USize i = 0; i < 4; ++i) {
+        nodes.push_back(Node::make_empty(next_indices[i], quads[i]));
+    }
+
+    return children_index;
 }
 
 template <FloatT Float>
-void BarnesHut<Float>::Node::self_recompute_com_mass() {
-    Float total_mass   = 0.0;
-    Vec2  weighted_sum = Vec2::make_zero();
+void BarnesHut<Float>::QuadTree::insert(Vec2 pos, Float mass) {
+    NodeIndex node_index = NODE_INDEX_EMPTY;
 
-    for (USize q = 0; q < 4; ++q) {
-        if (!children[q]) continue;
-        total_mass += children[q]->mass;
-        weighted_sum = weighted_sum.add(children[q]->center.scale(children[q]->mass));
+    while (nodes[node_index].is_internal()) {
+        Quad q     = nodes[node_index].quad.quad_from_pos(pos);
+        node_index = nodes[node_index].children_index + q;
     }
 
-    if (total_mass <= math::impl::default_epsilon<Float>()) {
-        mass   = 0.0;
-        center = Vec2::make_zero();
-    } else {
-        mass   = total_mass;
-        center = weighted_sum.scale(1.0 / total_mass);
-    }
-}
-
-template <FloatT Float>
-BarnesHut<Float>::QuadId BarnesHut<Float>::Node::impl_pos_quad_id(const Vec2& pos) const {
-    QuadId qid = 0;
-
-    if (pos.x >= quad_center.x) {
-        qid |= 1;
-    }
-
-    if (pos.y >= quad_center.y) {
-        qid |= 2;
-    }
-
-    return qid;
-}
-
-template <FloatT Float>
-BarnesHut<Float>::Vec2 BarnesHut<Float>::Node::impl_quad_id_center(QuadId qid) const {
-    Float half       = quad_radius / 2.0;
-    Vec2  new_center = Vec2::make_zero();
-
-    if (qid & 1) {
-        new_center.x = quad_center.x + half;
-    } else {
-        new_center.x = quad_center.x - half;
-    }
-
-    if (qid & 2) {
-        new_center.y = quad_center.y + half;
-    } else {
-        new_center.y = quad_center.y - half;
-    }
-
-    return new_center;
-}
-
-template <FloatT Float>
-void BarnesHut<Float>::Node::impl_apply_gravity_body_source(Body& body, const PointMass& pm,
-                                                            Float g, Float softening) const {
-    Vec2  delta   = pm.pos.sub(body.pm.pos);
-    Float r2_soft = delta.length_sq() + (softening * softening);
-
-    if (r2_soft <= math::impl::default_epsilon<Float>()) {
+    if (nodes[node_index].is_empty()) {
+        nodes[node_index].pos  = pos;
+        nodes[node_index].mass = mass;
         return;
     }
 
-    Float inv_r3              = 1.0 / (std::sqrt(r2_soft) * r2_soft);
-    Vec2  source_contribution = delta.scale(g * pm.mass * inv_r3);
-    body.acc                  = body.acc.add(source_contribution);
+    const Vec2  ex_pos  = nodes[node_index].pos;
+    const Float ex_mass = nodes[node_index].mass;
+
+    if (pos == ex_pos) {
+        nodes[node_index].mass += mass;
+        return;
+    }
+
+    while (true) {
+        const NodeIndex children_index = subdivide(node_index);
+
+        const Quad q1 = nodes[node_index].quad.quad_from_pos(ex_pos);
+        const Quad q2 = nodes[node_index].quad.quad_from_pos(pos);
+
+        if (q1 == q2) {
+            node_index = children_index + q1;
+        } else {
+            const NodeIndex n1 = children_index + q1;
+            const NodeIndex n2 = children_index + q2;
+
+            nodes[n1].pos  = ex_pos;
+            nodes[n1].mass = ex_mass;
+            nodes[n2].pos  = pos;
+            nodes[n2].mass = mass;
+
+            return;
+        }
+    }
 }
 
-template class BarnesHut<F32>;
-template class BarnesHut<F64>;
+template <FloatT Float>
+void BarnesHut<Float>::QuadTree::propagate_up_pos_mass() {
+    for (NodeIndex parent_index : parent_indices) {
+        const NodeIndex i = nodes[parent_index].children_index;
 
+        nodes[parent_index].pos =
+            nodes[i].pos.scale(nodes[i].mass) + nodes[i + 1].pos.scale(nodes[i + 1].mass) +
+            nodes[i + 2].pos.scale(nodes[i + 2].mass) + nodes[i + 3].pos.scale(nodes[i + 3].mass);
+
+        nodes[parent_index].mass =
+            nodes[i].mass + nodes[i + 1].mass + nodes[i + 2].mass + nodes[i + 3].mass;
+
+        nodes[parent_index].pos = nodes[parent_index].pos.scale(1.0 / nodes[parent_index].mass);
+    }
+}
+
+template <FloatT Float>
+BarnesHut<Float>::Vec2 BarnesHut<Float>::QuadTree::propagate_down_acc(Vec2 pos, Float g,
+                                                                      Float softening,
+                                                                      Float theta) const {
+    Vec2 acc = Vec2::make_zero();
+
+    const Float theta_sq     = theta * theta;
+    const Float softening_sq = softening * softening;
+
+    // NOTE: Start from the root node.
+    NodeIndex node_index = 0;
+
+    while (true) {
+        const Node& node            = nodes[node_index];
+        const Float dist            = node.pos.sub(pos);
+        const Float dist_sq         = dist.length_sq();
+        const bool  is_sd_satisfied = (node.quad.size * node.quad.size) < (theta_sq * dist_sq);
+
+        if (node.is_leaf() || is_sd_satisfied) {
+            const Float dist_norm = (dist_sq + softening_sq) * std::sqrt(dist_sq);
+
+            acc.x +=
+                std::min(g * node.mass * dist.x / dist_norm, std::numeric_limits<Float>::max());
+
+            acc.y +=
+                std::min(g * node.mass * dist.y / dist_norm, std::numeric_limits<Float>::max());
+
+            if (node.next_index == NODE_INDEX_EMPTY) {
+                break;
+            }
+
+            node_index = node.next_index;
+        } else {
+            node_index = node.children_index;
+        }
+    }
+
+    return acc;
+}
 }  // namespace nbody::sim
